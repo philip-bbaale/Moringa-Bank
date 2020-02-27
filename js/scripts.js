c04f3b7dc9f7eacd379d650baedf7195d7d70234
Pip Bbaale <namugera.philip@gmail.com>
2020-02-27 15:00:53 +0300

    final sorry for not regular commiting its kinda hectic

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var names, initialDeposit, depositAmount, withdrawAmount, totalDeposit,totalWithdraw, accountBalance;
+
+$(document).ready(function(){
+
+    $("form#registration").submit(function(){
+        event.preventDefault();
+        names = $("#exampleFormControlInput1").val();
+        initialDeposit = parseInt($("#exampleFormControlInput2").val());
+        accountBalance = initialDeposit;
+        $("#account_names").append(names);
+        $("#balance_statement").text('KCS: '+accountBalance);
+    });
+
+    $("#account_names").click(function(){
+        $(".funding").toggle();
+    });
+
+    $("form#deposits").submit(function(){
+        event.preventDefault();
+        depositAmount = parseInt($("#exampleFormControlInput3").val());
+        totalDeposit = accountBalance+depositAmount;
+        accountBalance = totalDeposit;
+        $("#balance_statement").text('KCS: '+accountBalance);
+    });
+    $("form#withdraws").submit(function(){
+        event.preventDefault();
+        withdrawAmount = parseInt($("#exampleFormControlInput4").val());
+        totalWithdraw = accountBalance-withdrawAmount;
+        accountBalance = totalWithdraw;
+        $("#balance_statement").text('KCS: '+accountBalance);
+    });
+});
